048f344c263a7a51023130e90333addfffad4d3c
fellowbeard <virgovibe@gmail.com>
2022-03-09 14:57:27 -1000

    created params controller method

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,5 +1,8 @@
 class ParamsController < ApplicationController
 
   def query_params
-    input_name = 
+    input_name = params["name"]
+    render json: {message: "Your name is #{input_name}"}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   
-  get "/params_game" => "params_examples#query_params""
+  get "/params_game" => "params#query_params"
 
 end
