2002ea93a5b5177e4c246cebe75cc0e961ecc67f
fellowbeard <virgovibe@gmail.com>
2022-03-09 16:30:16 -1000

    adds new route and controller methods

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -2,8 +2,12 @@ class ParamsController < ApplicationController
 
   def query_params
     input_name = params["name"]
-
     render json: {message: "Your name is #{input_name}", message2: "Your number is #{params['x']}"}
+  end
+
+  def url_segment
+    input_value = params["variable"]
+    render json: {message: "The url segment is #{input_value}"}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
   get "/query_params" => "params#query_params"
+  get "/url_segment_params/:variable" => "params#url_segment"
 
-  get "/"
+  get "/name" => "games#name_game"
+  get "/number_game" => "games#number_game"
 
 end
